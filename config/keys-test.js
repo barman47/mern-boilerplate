293a6d45da35a0013af6e1c0d9d3701b15fafcb1
Uzoanya Dominic <nomsouzoanya@yahoo.co.uk>
2019-12-26 21:10:15 +0100

    Initial Commit

---
+++ config/keys-test.js
@@ -0,0 +1,4 @@
+module.exports = {
+    database_URI: 'mongodb://localhost:27017/database_name',
+    secretOrKey: 'test secret'
+};
---
+++ routes/api/users.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// Register new user
+// @route POST /api/users/register
+// @desc register user
+// @access Public
+
+module.exports = router;
